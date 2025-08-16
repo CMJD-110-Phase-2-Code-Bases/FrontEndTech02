64f73c01a45cad8c92e5a7f557ba733d44ba998c
ThanuraSilva <thanurasilva1989@gmail.com>
2025-08-16 13:19:36 +0530

    Add table for course material component

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ import {MyComponent} from './MyComponent'
 import {NavBar} from '../src/components/NavBar'
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { CourseMaterial } from './components/CourseMaterial';
 
 function App() {
 
   return (
     <>
       <NavBar/>
+      <CourseMaterial/>
     </>
     
   );
